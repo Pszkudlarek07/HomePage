e396742f5357885d9f2ec3ca5de8b6aae4de04a8
Patryk Szkudlarek <patryk980207@gmail.com>
2024-02-16 11:02:32 +0100

    create footer

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 "use strict";
 
-console.log("test");
+// Button phone number functionality
+const button = document.getElementById("phone-nmbr");
+
+button.addEventListener("click", function () {
+  button.textContent = "📞 +48 501 023 188";
+});
