ffbc52db224be0bfe6f7577d83735bef38c219b1
Patryk Szkudlarek <patryk980207@gmail.com>
2024-02-19 20:56:47 +0100

    update whole file

--- script.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-// Button phone number functionality
-const button = document.getElementById("phone-nmbr");
-
-button.addEventListener("click", function () {
-  button.textContent = "📞 +48 501 023 188";
-});
