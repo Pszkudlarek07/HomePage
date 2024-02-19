09e0846deb24592403f96f967b6207778018edc1
Patryk Szkudlarek <patryk980207@gmail.com>
2024-02-19 21:02:17 +0100

    update whole folder

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// Button phone number functionality
+const button = document.getElementById("phone-nmbr");
+
+button.addEventListener("click", function () {
+  button.textContent = "📞 +48 501 023 188";
+});
